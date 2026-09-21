dd95bd5c2206ae4ff1ff590e37579ba71eff9d94
ivanbbs2001 <ivanbbs@gmail.com>
2026-09-21 12:19:20 -0300

    simulador apartamento cascavel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/investpro/',
-  server: {
-    port: 3000,
-    open: true
-  }
-})
+export default defineConfig({plugins:[react()],base:'/investpro/',server:{port:3000,open:true}})
